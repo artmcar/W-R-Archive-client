69d6ccee79f9e893250a82620678ae8c704862d6
Artur <makaryan.a.a@edu.mirea.ru>
2026-06-09 13:33:50 +0300

    Changed README.md

--- WRArchive/app/src/main/java/com/artmcar/wrarchive/data/remote/api/ApiConfig.kt
+++ WRArchive/app/src/main/java/com/artmcar/wrarchive/data/remote/api/ApiConfig.kt
@@ -1,6 +1,6 @@
 package com.artmcar.wrarchive.data.remote.api
 
 object ApiConfig {
-//    const val BASE_URL = "http://10.0.2.2:8080"
-    const val BASE_URL = "http://192.168.1.76:8080"
+    const val BASE_URL = "http://10.0.2.2:8080"
+//    const val BASE_URL = "http://192.168.1.76:8080"
 }
